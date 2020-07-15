8770f428118c22c5ab510b451cddb84efff0c4ae
Dominic Amato <dominic.a.amato@gmail.com>
2020-07-15 12:42:50 -0500

    Update MockModem.py

--- tests/MockModem.py
+++ tests/MockModem.py
@@ -1,5 +1,24 @@
+import sys
+
+sys.path.append(".")
+sys.path.append("..")
 from Hologram.Modem import Modem
+from Hologram.Utils import ModemResult
+
 
 class MockModem(Modem):
     def __init__(self):
-        
+        # dont init super class
+        self.device_name = 'Mock Modem'
+        self.baud_rate = 9600
+
+        self.serial_port = '/dev/ttyUSB0'
+        self.timeout = Modem.DEFAULT_SERIAL_TIMEOUT
+        self.response = []
+        self._at_sockets_available = False
+        self.urc_state = Modem.SOCKET_INIT
+        self.socket_identifier = 0
+        self.last_read_payload_length = 0
+        self.result = ModemResult.OK
+        self.debug_out = ''
+        self.in_ext = False
