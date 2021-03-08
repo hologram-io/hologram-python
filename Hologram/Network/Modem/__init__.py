0c24ceb80ff49ac8d25859b4674ed67090c633aa
Dom Amato <dom@hologram.io>
2021-03-08 16:54:34 -0600

    import the modem

--- Hologram/Network/Modem/__init__.py
+++ Hologram/Network/Modem/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ['Modem', 'MockModem', 'MS2131', 'Nova', 'E303', 'E372']
+__all__ = ['Modem', 'MockModem', 'MS2131', 'Nova', 'E303', 'E372', 'LE910']
 
 from .IModem import IModem
 from .Modem import Modem
